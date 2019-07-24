f54417405dd00214c7f2dc71903d45221f47b1e3
kierepkae <kierepkaedyta@gmail.com>
2019-07-24 15:29:06 +0100

    add test for release_bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,14 @@
 require 'docking_station'
 
 describe DockingStation do
+
   it " responds to release_bike" do
-        expect(subject).to respond_to :release_bike
-  end 
-end 
+    expect(subject).to respond_to :release_bike
+  end
+
+  it "release_bike return new bike" do
+    bike = Bike.new
+    expect(subject.release_bike).to eq bike
+  end
+
+end
