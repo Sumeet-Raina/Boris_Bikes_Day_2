23d930d4d13240d5a64142ef38c0f6ccec3ef278
sumeet978 <searchsumeet@gmail.com>
2019-07-24 15:42:25 +0100

    updated docking_station_spec.rb

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,11 @@
 require 'docking_station'
 
 describe DockingStation do
-
   it " responds to release_bike" do
     expect(subject).to respond_to :release_bike
   end
-
-
+  it " This gets a bike " do 
+     bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
